6dc64118df349eecee975f1afc9b368ed82a7ee8
Anatsutharsica <sutharsirajan@gmail.com>
2025-04-30 10:35:16 +0530

    Login page is added

---
+++ user-management-app/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold text-green-700">
+        This is my Dummy Dashboard!
+      </h1>
+    </div>
+  );
+};
+
+export default Dashboard;
